b4582be09992fdc45702b05b1eee35f7bdec0a07
Sandra Albrecht <sa454cam@gmail.com>
2022-07-04 12:35:03 +0100

    initial filtering options added to get endpoint

--- admin.py
+++ admin.py
@@ -1,21 +1,17 @@
+from datetime import datetime
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from flask_restful import Api, Resource
 from .config import postgresConn
+from werkzeug.exceptions import HTTPException
 
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = postgresConn
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
-api = Api(app)
-
-# # Generate database schema
-# Base.metadata.create_all(engine)
-# # Create new session
-# session = Session()
 
 class Manager(db.Model):
     __tablename__ = 'managers'
@@ -42,29 +38,67 @@ class Holiday(db.Model):
     holiday_end_date = db.Column(db.DateTime)
     number_of_holidays = db.Column(db.Numeric)
 
-@app.route('/pending-holidays')
-def get_pending_holidays():
-        try:
-                pending_holiday_request = Holiday.query.filter_by(status='pending').order_by(Holiday.created_at_date).all()
-                for req in pending_holiday_request:
-                        pending_holidays = jsonify(
-                                id=str(req.id), 
-                                author=str(req.employee_id), 
-                                status=str(req.status), 
-                                resolved_by=str(req.manager_id), 
-                                request_created_at=str(req.created_at_date), 
-                                vacation_start_date=str(req.holiday_start_date), 
-                                vacation_end_date=str(req.holiday_end_date),
-                                number_of_days_requested=str(req.number_of_holidays)
-                                )
-                return pending_holidays
-        except Exception as e:
-                # e holds description of the error
-                error_text = "<p>The error:<br>" + str(e) + "</p>"
-                hed = '<h1>Something is broken.</h1>'
-                return hed + error_text
-                # holiday_list = session.query(Holiday.status).filter(Holiday.status=='pending')
-                # return Holiday.dump(holiday_list)
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
+
+@app.route('/holidays', methods=['GET'])
+def get_holidays():
+    args = request.args()
+    page = args.get('page')
+    per_page = 5
+    id = args.get('id')
+    employee_id = args.get('employee_id')
+    manager_id = args.get('manager_id')
+    status = args.get('status')
+    created_at_date = args.get('created_at_date')
+    holiday_start_date = args.get('holiday_start_date')
+    holiday_end_date = args.get('holiday_end_date')
+    number_of_holidays = args.get('number_of_holidays')
+
+    filtered_holidays = Holiday.query.order_by(Holiday.created_at_date).all().paginate(page,per_page,error_out=False)
+    pending_holiday_list = []
+    for req in pending_holiday_request:
+        pending_holidays = {
+            'id': req.id,
+            'author': req.employee_id, 
+            'status': req.status, 
+            'resolved_by': req.manager_id, 
+            'request_created_at': req.created_at_date, 
+            'vacation_start_date': req.holiday_start_date, 
+            'vacation_end_date': req.holiday_end_date,
+            'number_of_days_requested': req.number_of_holidays
+        }
+        pending_holiday_list.append(pending_holidays)
+    return jsonify(result = pending_holiday_list)
+
+@app.route('/holiday-requests', methods=["POST"])
+def post_holiday_request():
+    data = json.loads(request.get_data())
+    employee_id = data['author']
+    status = data['status'] 
+    manager_id = data['resolved_by']
+    created_at_date = data['request_created_at']
+    holiday_start_date = data['vacation_start_date']
+    holiday_end_date = data['vacation_end_date']
+    number_of_holidays = data['number_of_days_requested']
+                                
+    new_request = Holiday(employee_id=employee_id, status=status, manager_id=manager_id, created_at_date=created_at_date, holiday_start_date=holiday_start_date, holiday_end_date=holiday_end_date, number_of_holidays=number_of_holidays)
+        
+    db.session.add(new_request)
+    db.session.commit()
+
+    return "Request has been added to the database", 201
+
 
 if __name__ == '__main__':
     app.run()
