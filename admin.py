e44e5f464d3b9684c5bf781840d01f0fbf444a88
Sandra Albrecht <sa454cam@gmail.com>
2022-07-05 09:57:03 +0100

    added PATCH endpoint

--- admin.py
+++ admin.py
@@ -51,63 +51,6 @@ def handle_exception(e):
     response.content_type = "application/json"
     return response
 
-@app.route('/holidays', methods=['GET'])
-def get_holidays():
-    args = request.args
-    page = args.get('page')
-    per_page = 5
-    id = args.get('id')
-    employee_id = args.get('employee_id')
-    manager_id = args.get('manager_id')
-    status = args.get('status')
-    created_at_date = args.get('created_at_date')
-    holiday_start_date = args.get('holiday_start_date')
-    holiday_end_date = args.get('holiday_end_date')
-    number_of_holidays = args.get('number_of_holidays')
-    query = Holiday.query
-
-    if id:
-        query = query.filter(Holiday.id == id)
-    
-    if employee_id:
-        query = query.filter(Holiday.employee_id == employee_id)
-    
-    if manager_id:
-        query = query.filter(Holiday.manager_id == manager_id)
-
-    if status:
-        query = query.filter(Holiday.status == status)
-
-    if created_at_date:
-        query = query.filter(Holiday.created_at_date == created_at_date)
-    
-    if holiday_start_date:
-        query = query.filter(Holiday.holiday_start_date == holiday_start_date)
-    
-    if holiday_end_date:
-        query = query.filter(Holiday.holiday_end_date == holiday_end_date)
-    
-    if number_of_holidays:
-        query = query.filter(Holiday.number_of_holidays == number_of_holidays)
-    
-    
-    holiday_query = query.order_by(created_at_date).paginate(page, per_page, False)
-    filtered_holiday_request = holiday_query.items
-    filtered_holiday_list = []
-    for req in filtered_holiday_request:
-        filtered_holidays = {
-            'id': req.id,
-            'author': req.employee_id, 
-            'status': req.status, 
-            'resolved_by': req.manager_id, 
-            'request_created_at': req.created_at_date, 
-            'vacation_start_date': req.holiday_start_date, 
-            'vacation_end_date': req.holiday_end_date,
-            'number_of_days_requested': req.number_of_holidays
-        }
-        filtered_holiday_list.append(filtered_holidays)
-    return jsonify(result = filtered_holiday_list)
-
 @app.route('/holiday-requests', methods=["POST"])
 def post_holiday_request():
     data = json.loads(request.get_data())
@@ -126,6 +69,68 @@ def post_holiday_request():
 
     return "Request has been added to the database", 201
 
+@app.route('/holidays', methods=['GET'])
+def get_holidays():
+    args = request.args
+    page = args.get('page')
+    per_page = 5
+    id = args.get('id')
+    employee_id = args.get('employee_id')
+    manager_id = args.get('manager_id')
+    status = args.get('status')
+    created_at_date = args.get('created_at_date')
+    holiday_start_date = args.get('holiday_start_date')
+    holiday_end_date = args.get('holiday_end_date')
+    number_of_holidays = args.get('number_of_holidays')
+    query = Holiday.query
+
+    if id:
+        query = query.filter(Holiday.id == id)
+    if employee_id:
+        query = query.filter(Holiday.employee_id == employee_id)
+    if manager_id:
+        query = query.filter(Holiday.manager_id == manager_id)
+    if status:
+        query = query.filter(Holiday.status == status)
+    if created_at_date:
+        query = query.filter(Holiday.created_at_date == created_at_date)
+    if holiday_start_date:
+        query = query.filter(Holiday.holiday_start_date == holiday_start_date)
+    if holiday_end_date:
+        query = query.filter(Holiday.holiday_end_date == holiday_end_date)
+    if number_of_holidays:
+        query = query.filter(Holiday.number_of_holidays == number_of_holidays)
+    
+    holiday_query = query.order_by(created_at_date).paginate(page, per_page, False)
+    filtered_holiday_request = holiday_query.items
+    filtered_holiday_list = []
+    for req in filtered_holiday_request:
+        filtered_holidays = {
+            'id': req.id,
+            'author': req.employee_id, 
+            'status': req.status, 
+            'resolved_by': req.manager_id, 
+            'request_created_at': req.created_at_date, 
+            'vacation_start_date': req.holiday_start_date, 
+            'vacation_end_date': req.holiday_end_date,
+            'number_of_days_requested': req.number_of_holidays
+        }
+        filtered_holiday_list.append(filtered_holidays)
+
+    return jsonify(result = filtered_holiday_list)
+
+@app.route('/holidays/<int:holiday_id>', methods=['PATCH'])
+def update_holiday(holiday_id):
+    data = json.loads(request.get_data())
+    status = data['status'] 
+                                
+    update_request = Holiday.query.get(holiday_id)
+    update_request.status = status
+        
+    db.session.add(update_request)
+    db.session.commit()
+
+    return "Holiday status has been updated", 200
 
 if __name__ == '__main__':
     app.run()
